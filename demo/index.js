78253794d60dbf6f9d8203feb1ba3b01a787c3ca
liuguanyu <lgy573@126.com>
2019-01-14 19:15:03 +0800

    -

--- demo/index.js
+++ demo/index.js
@@ -3,7 +3,19 @@ import App from './app.vue';
 
 import SRequest from 's-request';
 
-Vue.use(SRequest)
+import path from 'path';
+
+if (process.env.NODE_ENV === "development"){
+	let mock = require('./mock').default;
+
+	Vue.use(SRequest, {
+	    mocker: mock
+	});
+}
+else {
+	Vue.use(SRequest, {
+	});
+}
 
 new Vue({
     el: "#app",
